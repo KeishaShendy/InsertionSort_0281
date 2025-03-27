917eeca3186bb4017f1e0dce0a63176851ea79ca
keishashendy <shendyarrafli7@gmail.com>
2025-03-27 15:28:24 +0700

    Memperbaiki tampilan output dan menambahkan keterangan

--- file1.cpp
+++ file1.cpp
@@ -54,3 +54,12 @@ void insertionSort()
         arr[j + 1] = temp; // Step 5
     }
 }
+
+void display()
+{
+    cout << endl; // Output baris kosong
+    cout << "======================" << endl;
+    cout << "Total Passs = " << n - 1 << endl; // count element movement
+    cout << "======================" << endl; // Output ke layar
+    cout << "Element Array yang telah tersusun" << endl; // Output ke layar
+    cout << "======================" << endl; // Output ke layar
